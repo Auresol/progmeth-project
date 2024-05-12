4012acd13e7541965b6b3db5a3f5efaf458ce8c1
Suphawit <suphawit.promkhot@gmail.com>
2024-05-12 23:18:38 +0700

    Thor

--- src/control/GameControl.java
+++ src/control/GameControl.java
@@ -4,9 +4,11 @@ import component.*;
 
 import component.spell.BaseSpell;
 import component.spell.Fireball;
-import component.zerg.Baneling;
+import component.spell.LightningOrb;
+import component.spell.Tornado;
 import component.terran.Medic;
 import component.terran.Solider;
+import component.terran.Thor;
 import graphic.GameRender;
 import javafx.application.Platform;
 import util.Vector2D;
@@ -58,15 +60,10 @@ public class GameControl {
                 medic.setDirection(crystal.getPosition().subtract(initialPosition));
                 addEntity(Races.TERRAN, medic);
 
-//                initialPosition = new Vector2D();
-//                Thor thor = new Thor(initialPosition);
-//                medic.setDirection(crystal.getPosition().subtract(initialPosition));
-//                addEntity(Races.TERRAN, thor);
-
                 initialPosition = new Vector2D();
-                Baneling baneling = new Baneling(initialPosition);
+                Thor thor = new Thor(initialPosition);
                 medic.setDirection(crystal.getPosition().subtract(initialPosition));
-                addEntity(Races.TERRAN, baneling);
+                addEntity(Races.TERRAN, thor);
 
             }
         },0,4000);
