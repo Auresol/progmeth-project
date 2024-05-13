281496cee97057bf94018f085b3eb0f4ded0e7b0
Suphawit <suphawit.promkhot@gmail.com>
2024-05-14 00:39:50 +0700

    update gif

--- src/component/terran/Solider.java
+++ src/component/terran/Solider.java
@@ -15,7 +15,7 @@ public class Solider extends BaseTerranEnemy {
     private static final double IMAGE_SCALE = 0.3;
 
     public Solider(Vector2D position) {
-        super("Solider","solider.png", position, BASE_MAX_HEALTH,BASE_SPEED, IMAGE_SCALE, BASE_MIN_ATTACK_RANGE, BASE_MAX_ATTACK_RANGE, BASE_DAMAGE, BASE_ATTACK_FREQUENCY, Races.TERRAN);
+        super("Solider","Soldier.gif", position, BASE_MAX_HEALTH,BASE_SPEED, IMAGE_SCALE, BASE_MIN_ATTACK_RANGE, BASE_MAX_ATTACK_RANGE, BASE_DAMAGE, BASE_ATTACK_FREQUENCY, Races.TERRAN);
         setTarget(GameControl.getInstance().getPlayer());
     }
 
