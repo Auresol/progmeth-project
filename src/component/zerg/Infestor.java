bf44580fe65b88488ef62c3c3204bcaa623f8f0e
Suphawit <suphawit.promkhot@gmail.com>
2024-05-13 18:44:38 +0700

    Infestor

--- src/component/zerg/Infestor.java
+++ src/component/zerg/Infestor.java
@@ -1,4 +1,21 @@
 package component.zerg;
 
+import component.Races;
+import control.GameControl;
+import util.Vector2D;
+
 public class Infestor extends BaseZergEnemy{
+
+    private static final double BASE_MAX_HEALTH = 2.5;
+    private static final double BASE_DAMAGE = 1.5;
+    private static final double BASE_MIN_ATTACK_RANGE = 20;
+    private static final double BASE_MAX_ATTACK_RANGE = 20;
+    private static final double BASE_ATTACK_FREQUENCY = 1;
+    private static final double IMAGE_SCALE = 0.21;
+
+    public Infestor(Vector2D position) {
+        super("Infestor","Infestor.png", position, BASE_MAX_HEALTH,0, IMAGE_SCALE, BASE_MIN_ATTACK_RANGE, BASE_MAX_ATTACK_RANGE, BASE_DAMAGE, BASE_ATTACK_FREQUENCY, Races.TERRAN);
+        setTarget(GameControl.getInstance().getPlayer());
+        updateSprite();
+    }
 }
